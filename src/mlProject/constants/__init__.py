823123f21d330e96153ba0834acfe99b3b6197f9
Arpan Pal <arpanpalme@gmail.com>
2023-09-20 15:23:37 +0530

    ymal file reading

---
+++ src/mlProject/constants/__init__.py
@@ -0,0 +1,7 @@
+# Reading Yaml file
+from pathlib import Path
+
+
+CONFIG_FILE_PATH = Path("config/config.config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
